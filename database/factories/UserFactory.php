a57eb7b02b411e2fb0c602e6219295bd56f99d26
Makros <lorenterodriguezmarcos@gmail.com>
2024-06-26 15:22:27 +0200

    Move model User to Shared folder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,14 +3,14 @@
 namespace Database\Factories;
 
 use App\Models\Team;
-use App\Models\User;
+use Domain\Shared\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Shared\Models\User>
  */
 class UserFactory extends Factory
 {
