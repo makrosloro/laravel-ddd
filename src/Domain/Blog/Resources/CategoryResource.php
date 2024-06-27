fcc8b22729196359f0491bce102b732bf37cb8e7
Makros <lorenterodriguezmarcos@gmail.com>
2024-06-27 11:40:47 +0200

    New project model resources

---
+++ src/Domain/Blog/Resources/CategoryResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Domain\Blog\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+
+}
---
+++ src/Domain/Blog/Resources/PostResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Domain\Blog\Resources;
+
+use Domain\Shared\Resources\UserResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'tags' => TagResource::collection($this->whenLoaded('tags')),
+            'created_at' => $this->created_at->isoFormat('LL'),
+        ];
+    }
+}
